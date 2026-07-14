672cb21bfd92dfaf222d829eb8481397f1934b5d
Sohan Maity <sohanmaity5272@gmail.com>
2026-07-14 19:25:34 +0530

    path resolved from frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route,Navigate } from "react-router";
 import Homepage from "./pages/Homepage";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import { useDispatch, useSelector } from 'react-redux';
+import { checkAuth } from "./authSlice";
+import { useEffect } from "react";
+
 
 function App(){
+
+   const dispatch = useDispatch();
+    const {isAuthenticated,user,loading} = useSelector((state)=>state.auth);
+  
+    // check initial authentication
+    useEffect(() => {
+      dispatch(checkAuth());
+    }, [dispatch]);
+
     return(
     <Routes>
-      <Route path="/" element={<Homepage></Homepage>}></Route>
-      <Route path="/login" element={<Login></Login>}></Route>
-      <Route path="/signup" element={<Signup></Signup>}></Route>
+      <Route path="/" element={isAuthenticated ?<Homepage></Homepage>:<Navigate to="/signup" />}></Route>
+      <Route path="/login" element={isAuthenticated?<Navigate to="/" />:<Login></Login>}></Route>
+      <Route path="/signup" element={isAuthenticated?<Navigate to="/" />:<Signup></Signup>}></Route>
     </Routes>
     )
 }
