617884e24c33051d7e72d480a35d9160a990238f
Sohan Maity <sohanmaity5272@gmail.com>
2026-07-23 22:51:04 +0530

    problem page

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
-import { Routes, Route,Navigate } from "react-router";
-import Homepage from "./pages/Homepage";
-import Signup from "./pages/Signup";
+import {Routes, Route ,Navigate} from "react-router";
 import Login from "./pages/Login";
-import AdminPanel from "./pages/AdminPanel"
+import Signup from "./pages/Signup";
+import Homepage from "./pages/Homepage";
 import { useDispatch, useSelector } from 'react-redux';
 import { checkAuth } from "./authSlice";
 import { useEffect } from "react";
+import AdminPanel from "./components/AdminPanel";
+import ProblemPage from "./pages/ProblemPage"
+import Admin from "./pages/Admin";
+import AdminDelete from "./components/AdminDelete"
 
-
-function App() {
+function App(){
+  
   const dispatch = useDispatch();
-  const { isAuthenticated, loading } = useSelector(
-    (state) => state.auth
-  );
+  const {isAuthenticated,user,loading} = useSelector((state)=>state.auth);
 
+  // check initial authentication
   useEffect(() => {
     dispatch(checkAuth());
   }, [dispatch]);
-
- if (loading) {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
+  
+  if (loading) {
+    return <div className="min-h-screen flex items-center justify-center">
       <span className="loading loading-spinner loading-lg"></span>
-    </div>
-  );
-}
+    </div>;
+  }
 
-  return (
+  return(
+  <>
     <Routes>
-      <Route path="/" element={ isAuthenticated ? <Homepage /> : <Navigate to="/signup" replace />}/>
-      <Route path="/login" element={ isAuthenticated ? <Navigate to="/" replace /> : <Login /> }/>
-      <Route path="/signup" element={  isAuthenticated ? <Navigate to="/" replace /> : <Signup /> }/>
-      <Route path="/admin" element={<AdminPanel/>}></Route>
-    </Routes >
-  );
+      <Route path="/" element={isAuthenticated ?<Homepage></Homepage>:<Navigate to="/signup" />}></Route>
+      <Route path="/login" element={isAuthenticated?<Navigate to="/" />:<Login></Login>}></Route>
+      <Route path="/signup" element={isAuthenticated?<Navigate to="/" />:<Signup></Signup>}></Route>
+      <Route path="/admin" element={isAuthenticated && user?.role === 'admin' ? <Admin /> : <Navigate to="/" />} />
+      <Route path="/admin/create" element={isAuthenticated && user?.role === 'admin' ? <AdminPanel /> : <Navigate to="/" />} />
+      <Route path="/admin/delete" element={isAuthenticated && user?.role === 'admin' ? <AdminDelete /> : <Navigate to="/" />} />
+      <Route path="/problem/:problemId" element={<ProblemPage/>}></Route>
+      
+    </Routes>
+  </>
+  )
 }
 
 export default App;
